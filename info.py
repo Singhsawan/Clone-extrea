e98b5569c5843c29d4d4a6e81b7aed767a3e0618
Sridhar.k <138989644+Sridhark01@users.noreply.github.com>
2023-07-08 20:21:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,6 +19,10 @@ API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
+#rename
+FLOOD = int(environ.get("FLOOD", "10"))
+RENAME_MODE = bool(environ.get("RENAME_MODE"))
+
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
